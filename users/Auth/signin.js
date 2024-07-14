8d82e3d286ef37d71632faafaa8515e18e6f5096
Meet <meetmendapara09@gmail.com>
2024-07-14 11:13:03 +0530

    manage the api key

--- users/Auth/signin.js
+++ users/Auth/signin.js
@@ -8,13 +8,13 @@ import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstati
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-        apiKey: "AIzaSyAWDj26y_gIE4-Y7dqXbA3ECyEDcSJU4-Y",
-        authDomain: "odoo-f2f87.firebaseapp.com",
-        projectId: "odoo-f2f87",
-        storageBucket: "odoo-f2f87.appspot.com",
-        messagingSenderId: "974181498130",
-        appId: "1:974181498130:web:6668aa9a80bdd14bbbc7cb",
-        measurementId: "G-DFGBG91YQ8"
+        apiKey: FIREBASE_API_KEY,
+        authDomain: FIREBASE_AUTH_DOMAIN,
+        projectId: FIREBASE_PROJECT_ID,
+        storageBucket: FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+        appId: FIREBASE_APP_ID,
+        measurementId: FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
