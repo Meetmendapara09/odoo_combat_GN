c9be18e9654c90c59f9c21f26798113a0ad79f31
Harshils411 <harshilsolanki114@gmail.com>
2024-07-14 09:47:03 +0530

    navbar

--- new/LMS/src/App.jsx
+++ new/LMS/src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
+import Navbar from './Component/Navbar';
 
 function App() {
   return (
     <>
-      <nav className='flex justify-between'>
-        {/* <img></img> */}
-        <h1 className='text-2xl'>Title</h1>
-        <button className=' border-red-700 mr-5'>Login</button>
-      </nav>
+   <Navbar/>
 
     </>
   );
