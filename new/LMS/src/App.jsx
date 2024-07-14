0b25879ab537d28b82c1a9d8c27af2713999ef88
Harshils411 <harshilsolanki114@gmail.com>
2024-07-14 12:33:17 +0530

    update the current filesystem

--- new/LMS/src/App.jsx
+++ new/LMS/src/App.jsx
@@ -1,14 +1,12 @@
-// <<<<<<< HEAD
 import React from "react";
 import Navbar from "./Component/Navbar";
 import Cart from "./components/Cart";
-// >>>>>>> 623a473c67db8e2b9d934f10aff6729cb39eeaba
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* <Cart/> */}
+      <Cart/>
     </>
   );
 }
--- new/LMS/src/Auth/Signin.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Signin() {
-  return <></>;
-}
-
-export default Signin;
