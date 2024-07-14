ba8bcba5ae3b17a930a7ec3ee1ce9f3e76ae03cb
Meet <meetmendapara09@gmail.com>
2024-07-14 09:43:01 +0530

    update the files

--- new/LMS/src/App.jsx
+++ new/LMS/src/App.jsx
@@ -1,5 +1,6 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from 'react'
-
+import Cart from './components/Cart'
 function App() {
   return (
     <>
@@ -8,7 +9,11 @@ function App() {
         <h1 className='text-2xl'>Title</h1>
         <button className=' border-red-700 mr-5'>Login</button>
       </nav>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
