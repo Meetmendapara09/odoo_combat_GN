70feeefa92ff93788ad3d42413bc455777ad182c
Harshils411 <harshilsolanki114@gmail.com>
2024-07-14 12:36:29 +0530

    update the current state of the file system

--- new/LMS/src/App.jsx
+++ new/LMS/src/App.jsx
@@ -1,20 +1,14 @@
-<<<<<<< HEAD
 import React from "react";
 import Navbar from "./Component/Navbar";
 import Cart from "./components/Cart";
-=======
 import BookDetails from './Books/BookDetails';
 import './App.css';
->>>>>>> b5552d2ca03f92836509fe662643fb1f183245fd
 
 const App = () => {
   return (
-<<<<<<< HEAD
     <>
       <Navbar />
       <Cart/>
-    </>
-=======
     <div className="App">
       <header className="App-header">
         <h1>Library Management System</h1>
@@ -23,7 +17,7 @@ const App = () => {
         <BookDetails />
       </main>
     </div>
->>>>>>> b5552d2ca03f92836509fe662643fb1f183245fd
+    </>
   );
 };
 
